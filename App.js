5fcb6cb20898b5425c0cb6a2db28bb19bbc29ac0
Eumericano <opwer032@naver.com>
2022-05-10 23:49:02 +0900

    feat: Set prefetch images and font

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import react, { useState } from "react";
+import { useState } from "react";
 import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
 import { Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 export default function App() {
   const [ready, setReady] = useState(false);
 
   const startLoading = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    // 로컬 ttf 파일들도 load할 수 있다.
+    await Font.loadAsync(Ionicons.font);
+
+    await Asset.loadAsync(require("./seulgi.jpeg"));
+    // url을 이용한 이미지는 prefetch로 접근
+    await Image.prefetch(
+      "https://nomad-sexy-coffee.s3.ap-northeast-2.amazonaws.com/shops/u2vkaaj8o-kzkpvegr-%E1%84%89%E1%85%B3%E1%86%AF%E1%84%80%E1%85%B52.jpeg"
+    );
   };
   const onFinish = () => setReady(true);
 
