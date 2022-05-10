0ca4786b20f3b2423c29697242257c3372ab828f
Eumericano <opwer032@naver.com>
2022-05-11 08:22:41 +0900

    refactor: Organize preload lib

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { useState } from "react";
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
-import { Text } from "react-native";
+import { Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset } from "expo-asset";
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
+
 export default function App() {
   const [ready, setReady] = useState(false);
 
   const startLoading = async () => {
     // 로컬 ttf 파일들도 load할 수 있다.
-    await Font.loadAsync(Ionicons.font);
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require("./seulgi.jpeg"),
+      "https://nomad-sexy-coffee.s3.ap-northeast-2.amazonaws.com/shops/u2vkaaj8o-kzkpvegr-%E1%84%89%E1%85%B3%E1%86%AF%E1%84%80%E1%85%B52.jpeg",
+    ]);
 
-    await Asset.loadAsync(require("./seulgi.jpeg"));
-    // url을 이용한 이미지는 prefetch로 접근
-    await Image.prefetch(
-      "https://nomad-sexy-coffee.s3.ap-northeast-2.amazonaws.com/shops/u2vkaaj8o-kzkpvegr-%E1%84%89%E1%85%B3%E1%86%AF%E1%84%80%E1%85%B52.jpeg"
-    );
+    await Promise.all([...fonts, ...images]);
   };
   const onFinish = () => setReady(true);
 
