ed776545b7caf87952d4bbd69f0f0f24d59e379e
Eumericano <opwer032@naver.com>
2022-05-13 15:33:11 +0900

    learn: Add styles on navigators

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { View, Text } from "react-native-web";
 
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
@@ -8,21 +9,34 @@ const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
   <Tab.Navigator
+    //screenOption : 네비게이션의 모든 헤더에 스타일 적용
     screenOptions={{
-      tabBarLabelStyle: {
+      tabBarStyle: {
         backgroundColor: "black",
-        color: "white",
+        paddingHorizontal: 3,
+        paddingVertical: 3,
       },
+      tabBarActiveTintColor: "red",
+      tabBarInactiveTintColor: "white",
     }}
   >
-    <Tab.Screen name="Movies" component={Movies} />
     <Tab.Screen
-      name="Tv"
-      component={Tv}
+      name="Movies"
+      component={Movies}
+      //option : 해당 스크린에만 스타일 적용
       options={{
-        tabBarLabelStyle: { backgroundColor: "purple", color: "white" },
+        headerTitleContainerStyle: {
+          backgroundColor: "black",
+          color: "tomato",
+        },
+        headerRight: () => {
+          <View>
+            <Text>Hello</Text>
+          </View>;
+        },
       }}
     />
+    <Tab.Screen name="Tv" component={Tv} />
     <Tab.Screen name="Search" component={Search} />
   </Tab.Navigator>
 );
