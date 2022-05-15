912ad73edb50ea6cdf66a3c794e263c55388254f
Eumericano <opwer032@naver.com>
2022-05-15 13:15:07 +0900

    chore: Add native stack navigator

---
+++ navigation/Stack.js
@@ -0,0 +1,7 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const NativeStack = createNativeStackNavigator();
+
+const Stack = () => <NativeStack.Navigator></NativeStack.Navigator>;
+
+export default Stack;
