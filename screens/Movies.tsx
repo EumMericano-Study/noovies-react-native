c03bd97e1b34d3868e65daa3cb039ea3f9c7d317
Eumericano <opwer032@naver.com>
2022-05-18 21:27:14 +0900

    feat: Add movie now playing api

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,12 +1,42 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import styled from "../styled-components";
+//높이를 알기 위해 Dimensions 이용
+import { Dimensions } from "react-native";
+import Swiper from "react-native-web-swiper";
 
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => (
-  <Container></Container>
-);
+const API_KEY = "78623a14ff23a512a97109e77e1151dc";
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const getNowPlaying = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+    );
+  };
+
+  return (
+    <Container>
+      <Swiper
+        loop
+        timeout={3.5}
+        controlsEnabled={false}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 3.5 }}
+      >
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+        <View style={{ backgroundColor: "white" }}></View>
+        <View style={{ backgroundColor: "brown" }}></View>
+      </Swiper>
+    </Container>
+  );
+};
 
 export default Movies;
 
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+const View = styled.View`
+  flex: 1;
 `;
