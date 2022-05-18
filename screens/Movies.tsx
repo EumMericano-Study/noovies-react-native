5fa412a32848d97c010e75a02a6d933a56324cf8
Eumericano <opwer032@naver.com>
2022-05-18 22:59:18 +0900

    feat: Add loading state

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,20 +1,26 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import styled from "../styled-components";
 //높이를 알기 위해 Dimensions 이용
-import { Dimensions } from "react-native";
+import { ActivityIndicator, Dimensions } from "react-native";
 import Swiper from "react-native-web-swiper";
+import { useState } from "react";
 
 const API_KEY = "78623a14ff23a512a97109e77e1151dc";
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const [loading, setLoading] = useState<boolean>(true);
   const getNowPlaying = () => {
     fetch(
       `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
     );
   };
 
-  return (
+  return loading ? (
+    <Loader>
+      <ActivityIndicator />
+    </Loader>
+  ) : (
     <Container>
       <Swiper
         loop
@@ -33,10 +39,14 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
 
 export default Movies;
 
-const Container = styled.ScrollView`
-  background-color: ${(props) => props.theme.mainBgColor};
-`;
+const Container = styled.ScrollView``;
 
 const View = styled.View`
   flex: 1;
+`;
+
+const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 `;
