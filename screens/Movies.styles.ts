46a5becca5c7070c7b8a06f5cfc8e9cf1d53078a
Eumericano <opwer032@naver.com>
2022-05-22 16:42:23 +0900

    refactor: Devide styled components

---
+++ screens/Movies.styles.ts
@@ -0,0 +1,27 @@
+import styled from "../styled-components";
+
+export const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ListTitle = styled.Text`
+  color: white;
+  font-size: 18px;
+  font-weight: 600;
+
+  margin-left: 30px;
+`;
+
+export const TrendingScroll = styled.FlatList`
+  margin-top: 20px;
+`;
+
+export const ListContianer = styled.View`
+  margin-bottom: 40px;
+`;
+
+export const ComingSoonTitle = styled(ListTitle)`
+  margin-bottom: 20px;
+`;
--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import styled from "../styled-components";
 
 //높이를 알기 위해 Dimensions 이용
-import { ActivityIndicator, Dimensions, FlatList, View } from "react-native";
+import { ActivityIndicator, Dimensions, FlatList } from "react-native";
 import Swiper from "react-native-swiper";
 import Slide from "../components/Slide";
 import { renderVMedia } from "../components/VMedia";
 import { renderHMedia } from "../components/HMedia";
 import { HSeparator, VSeparator } from "../components/Separators";
+import { keyExtractor } from "../utils";
+import {
+  ComingSoonTitle,
+  ListContianer,
+  ListTitle,
+  Loader,
+  TrendingScroll,
+} from "./Movies.styles";
 
 const API_KEY = "78623a14ff23a512a97109e77e1151dc";
 
@@ -108,47 +115,21 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
               horizontal
               showsHorizontalScrollIndicator={false}
               contentContainerStyle={{ paddingLeft: 30 }}
-              ItemSeparatorComponent={() => <VSeparator />}
+              ItemSeparatorComponent={VSeparator}
               data={trending}
-              keyExtractor={(item) => `${item.id}`}
+              keyExtractor={keyExtractor}
               renderItem={renderVMedia}
             />
           </ListContianer>
           <ComingSoonTitle>Coming soon</ComingSoonTitle>
         </>
       }
-      ItemSeparatorComponent={() => <HSeparator />}
+      ItemSeparatorComponent={HSeparator}
       data={upcoming}
-      keyExtractor={(item) => `${item.id}`}
+      keyExtractor={keyExtractor}
       renderItem={renderHMedia}
     />
   );
 };
 
 export default Movies;
-
-const Loader = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ListTitle = styled.Text`
-  color: white;
-  font-size: 18px;
-  font-weight: 600;
-
-  margin-left: 30px;
-`;
-
-const TrendingScroll = styled.FlatList`
-  margin-top: 20px;
-`;
-
-const ListContianer = styled.View`
-  margin-bottom: 40px;
-`;
-
-const ComingSoonTitle = styled(ListTitle)`
-  margin-bottom: 20px;
-`;
