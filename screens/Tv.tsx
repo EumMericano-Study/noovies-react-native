9b8dea638f533e368ac33971dd3b17f6f018fd2b
Eumericano <opwer032@naver.com>
2022-05-24 08:12:54 +0900

    feat: Add layouts with apis

--- screens/Tv.tsx
+++ screens/Tv.tsx
@@ -1,9 +1,63 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, FlatList } from "react-native";
+import { useQuery } from "react-query";
+import { tvApi } from "../api";
+import Loader from "../components/Loader";
+import VMedia, { renderVMedia } from "../components/VMedia";
 
-const Tv = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text>Tv</Text>
-  </View>
-);
+const Tv = () => {
+  const { isLoading: todayLoading, data: todayData } = useQuery(
+    ["tv", "today"],
+    tvApi.airingToday
+  );
+  const { isLoading: topLoading, data: topData } = useQuery(
+    ["tv", "top"],
+    tvApi.topRated
+  );
+  const { isLoading: trendingLoading, data: trendingData } = useQuery(
+    ["tv", "trending"],
+    tvApi.trending
+  );
+
+  const loading = todayLoading || topLoading || trendingLoading;
+
+  if (loading) return <Loader />;
+  return (
+    <ScrollView>
+      <FlatList
+        horizontal
+        data={todayData.results}
+        renderItem={({ item }) => (
+          <VMedia
+            originalTitle={item.original_name}
+            posterPath={item.poster_path}
+            voteAverage={item.vote_average}
+          />
+        )}
+      />
+      <FlatList
+        horizontal
+        data={trendingData.results}
+        renderItem={({ item }) => (
+          <VMedia
+            originalTitle={item.original_name}
+            posterPath={item.poster_path}
+            voteAverage={item.vote_average}
+          />
+        )}
+      />
+      <FlatList
+        horizontal
+        data={topData.results}
+        renderItem={({ item }) => (
+          <VMedia
+            originalTitle={item.original_name}
+            posterPath={item.poster_path}
+            voteAverage={item.vote_average}
+          />
+        )}
+      />
+    </ScrollView>
+  );
+};
 
 export default Tv;
