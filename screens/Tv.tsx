3c1e350e50c6f5825d8a2f8672c15aed09113b85
Eumericano <opwer032@naver.com>
2022-05-24 08:58:45 +0900

    feat: Add tv refreshing using queryclient

--- screens/Tv.tsx
+++ screens/Tv.tsx
@@ -1,28 +1,42 @@
-import { ScrollView } from "react-native";
-import { useQuery } from "react-query";
+import { RefreshControl, ScrollView } from "react-native";
+import { QueryClient, useQuery } from "react-query";
 import { tvApi } from "../api";
 import HList from "../components/HList";
 import Loader from "../components/Loader";
 
 const Tv = () => {
-  const { isLoading: todayLoading, data: todayData } = useQuery(
-    ["tv", "today"],
-    tvApi.airingToday
-  );
-  const { isLoading: topLoading, data: topData } = useQuery(
-    ["tv", "top"],
-    tvApi.topRated
-  );
-  const { isLoading: trendingLoading, data: trendingData } = useQuery(
-    ["tv", "trending"],
-    tvApi.trending
-  );
+  const queryClient = new QueryClient();
+
+  const {
+    isLoading: todayLoading,
+    data: todayData,
+    isRefetching: todayRefetching,
+  } = useQuery(["tv", "today"], tvApi.airingToday);
+  const {
+    isLoading: topLoading,
+    data: topData,
+    isRefetching: topRefetching,
+  } = useQuery(["tv", "top"], tvApi.topRated);
+  const {
+    isLoading: trendingLoading,
+    data: trendingData,
+    isRefetching: trendingRefetching,
+  } = useQuery(["tv", "trending"], tvApi.trending);
+  const onRefresh = () => {
+    queryClient.refetchQueries(["tv"]);
+  };
 
   const loading = todayLoading || topLoading || trendingLoading;
+  const refreshing = todayRefetching || topRefetching || trendingRefetching;
 
   if (loading) return <Loader />;
   return (
-    <ScrollView contentContainerStyle={{ paddingVertical: 30 }}>
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+      contentContainerStyle={{ paddingVertical: 30 }}
+    >
       <HList title="Trending TV" data={trendingData.results} />
       <HList title="Airing Today" data={todayData.results} />
       <HList title="Top Rated TV" data={topData.results} />
